bb14cf0a4277f55e1895960d5d90f120128d1295
dnagula <dnagula@arinc.com>
2016-04-07 11:07:05 -0500

    updating changes

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,6 +2,12 @@
 
 // Loads the standardBuild function/step from workflowLibs.git/vars/standardBuild.groovy
 // and invokes it.
+
+stage 'Checkout'
+stage 'Build'
+stage 'QA'
+stage 'Production'
+
 standardBuild {
     environment = 'golang:1.5.0'
     mainScript = '''
--- standardBuild.groovy
+++ standardBuild.groovy
@@ -1,7 +1,4 @@
-// See https://github.com/jenkinsci/workflow-plugin/tree/master/cps-global-lib#defining-global-functions
 
-// The call(body) method in any file in workflowLibs.git/vars is exposed as a
-// method with the same name as the file.
 def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
